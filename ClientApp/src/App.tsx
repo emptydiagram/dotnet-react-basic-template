7e5cad37fb43bedf19a88a9101cc8fbec8397ce9
emptydiagram <llamachannel@fastmail.com>
2022-05-04 21:39:52 -0400

    Add react-router-dom v6 and routing demo

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import * as React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Demo</p>
       </header>
+      <h1>Bookkeeper</h1>
+      <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
+        <Link to="/invoices">Invoices</Link> |{' '}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
-
-export default App;

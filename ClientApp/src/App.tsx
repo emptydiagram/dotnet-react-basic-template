ab60b66164c9eef843ff385d77a98e159b1b10ed
emptydiagram <emptydiagram@fastmail.com>
2022-05-04 23:56:35 -0400

    Add Box and Container

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,19 +1,33 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import * as React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Demos</p>
-      </header>
-      <nav style={{ borderBottom: 'solid 1px', padding: '1rem 0' }}>
-        <Link to="/invoices">Invoices</Link> |{' '}
-        <Link to="/adder">Adder</Link> |{' '}
-        <Link to="/snackbar">Snackbar</Link>
-      </nav>
-      <Outlet />
-    </div>
+    <Container maxWidth="sm">
+      <div className="App">
+        <header className="App-header">
+          <Box
+            sx={{
+              width: '100%',
+              backgroundColor: 'primary.dark',
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+          }}>
+            <p>Demos</p>
+          </Box>
+        </header>
+        <nav style={{ borderBottom: 'solid 1px', padding: '1rem 0' }}>
+          <Link to="/invoices">Invoices</Link> |{' '}
+          <Link to="/adder">Adder</Link> |{' '}
+          <Link to="/snackbar">Snackbar</Link>
+        </nav>
+        <Outlet />
+      </div>
+    </Container>
   );
 }

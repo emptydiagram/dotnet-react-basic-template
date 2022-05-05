8f9d806bad55b4276f5bb525264a5eadf03f7866
emptydiagram <emptydiagram@fastmail.com>
2022-05-05 01:23:04 -0400

    Add BloggingContext model, migrations and sample CRUD statements from EF Core tutorial

--- Program.cs
+++ Program.cs
@@ -1,27 +1,65 @@
-var builder = WebApplication.CreateBuilder(args);
+using dotnet_react_basic_template;
 
-// Add services to the container.
+var runApp = () => {
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+    // Add services to the container.
 
-var app = builder.Build();
+    builder.Services.AddControllersWithViews();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    // app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+
+    app.MapFallbackToFile("index.html");;
+
+    app.Run();
+};
+
+//////////
+
+using (var db = new BloggingContext())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // Note: This sample requires the database to be created before running.
+    Console.WriteLine($"Database path: {db.DbPath}.");
+
+    // Create
+    Console.WriteLine("Inserting a new blog");
+    db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+    db.SaveChanges();
+
+    // Read
+    Console.WriteLine("Querying for a blog");
+    var blog = db.Blogs
+        .OrderBy(b => b.BlogId)
+        .First();
+
+    // Update
+    Console.WriteLine("Updating the blog and adding a post");
+    blog.Url = "https://devblogs.microsoft.com/dotnet";
+    blog.Posts.Add(
+        new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+    db.SaveChanges();
+
+    // Delete
+    Console.WriteLine("Delete the blog");
+    db.Remove(blog);
+    db.SaveChanges();
 }
 
-// app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
 
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");;
-
-app.Run();
+runApp();
